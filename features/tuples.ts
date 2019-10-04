d5a7ad790537d3defb165bd149bb9178362d98f2
Martin Kretzschmar <martin@jp7.com.br>
2019-10-04 11:28:02 -0300

    More on tuples

--- features/tuples.ts
+++ features/tuples.ts
@@ -4,7 +4,7 @@ const drink = {
   sugar: 40
 };
 
-// Ideia of a tuple, make it reusable
+// Type alias. Ideia of a tuple, make it reusable.
 type Drink = [string, boolean, number];
 
 // Annotating order to tuple types
@@ -13,3 +13,10 @@ const pepsi: Drink = ['brown', true, 40];
 
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['green', false, 0];
+
+// ==
+const carSpecs: [number, number] = [400, 3354];
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+};
