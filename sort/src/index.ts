44500fa8a6d4edfd6c56256c2de42f56151fecb7
Martin Kretzschmar <martin@jp7.com.br>
2020-01-20 14:47:43 -0300

    Sorter project with TypeScript

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,22 +1,22 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-  sort(): void {
-    const { length } = this.collection;
+/** NUMBERS COLLECTION */
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j+1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j+1];
-          this.collection[j+1] = leftHand;
-        }
-      }
-    }
-  }
-};
+/** CHARACTER COLLECTION */
+const characterCollection = new CharactersCollection('zadsQq');
+characterCollection.sort();
+console.log(characterCollection.data);
 
-const sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-
-console.log(sorter.collection);
+/** LINKED LIST */
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
